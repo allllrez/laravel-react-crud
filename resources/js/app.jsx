9d933d53fdfa195973ee2eb418d0fcfa9c514a54
allllrez <anabestanireza@yahoo.com>
2024-11-09 11:09:35 +0330

    laravel react simple crun app

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "toastr/build/toastr.css";
+
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import App from "./Pages/App.jsx";
+
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
+
